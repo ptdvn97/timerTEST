c6afbda3f3d1ef561cb436fef6d901a6e8bb37fb
ptdvn97 <ptdvn97@gmail.com>
2017-10-25 12:25:17 +0700

    Initial commit

---
+++ main.c
@@ -0,0 +1,60 @@
+#include <main.h>
+
+#define BUT_1   input(PIN_B4)
+#define BUT_2   input(PIN_B5)
+
+int8 Q[]={0x7E,0xBD,0xDB,0xE7};
+int a=0,b=0,c=0,d=0;
+#INT_RB
+void interrupt()
+{
+   while(1)
+   {
+   if(BUT_1==1)
+   {
+      a=1;
+      while(BUT_1==1)
+         {delay_ms(1);}
+      while(a==1)
+      {
+         OUTPUT_BIT(PIN_D5,1);
+         
+         OUTPUT_BIT(PIN_D6,0);
+         if(BUT_2==1)
+            {
+               a=0;
+               break;
+               while(BUT_2==0)
+         {delay_ms(1);}
+            }
+      }
+   }
+   if(BUT_2==1)
+   {
+      b=1;
+      while(BUT_2==1)
+         {delay_ms(1);}
+      while(b==1)
+      {
+         
+         OUTPUT_BIT(PIN_D5,0);
+         
+         OUTPUT_BIT(PIN_D6,1);
+         if(BUT_1==0)
+            {
+               b=0;
+               break;
+               while(BUT_1==1)
+         {delay_ms(1);}
+            }
+      }
+   }
+}}
+
+void main()
+{
+   output_d(0x00);
+   clear_interrupt(INT_RB);
+   enable_interrupts(INT_RB);
+   enable_interrupts(GLOBAL);  
+}
